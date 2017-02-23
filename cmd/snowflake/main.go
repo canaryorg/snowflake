8ab8dce70369322c08c45b4742f109bf754c5dd5
savaki <matt.ho@gmail.com>
2017-02-23 14:56:43 -0800

    - removed support for pulling data from aws - really no need for this

--- cmd/snowflake/main.go
+++ cmd/snowflake/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
-	"time"
 
 	"github.com/savaki/snowflake/snowstorm"
 )
@@ -17,46 +13,27 @@ type UserData struct {
 }
 
 type Options struct {
-	ServerId int
+	ServerID int
 	Port     int
-	AWS      bool
 }
 
 func main() {
 	var (
 		flagServerID = flag.Int("id", 0, "unique server id")
 		flagPort     = flag.Int("port", 7006, "port to list on")
-		flagNoAWS    = flag.Bool("no-aws", false, "do not attempt to retrieve server id from user data json")
 	)
 	flag.Parse()
 
 	opts := Options{
-		ServerId: *flagServerID,
+		ServerID: *flagServerID,
 		Port:     *flagPort,
-		AWS:      !*flagNoAWS,
 	}
 
 	Run(opts)
 }
 
 func Run(opts Options) {
-	serverID := opts.ServerId
-
-	if opts.AWS {
-		ctx, _ := context.WithTimeout(context.Background(), time.Second*3)
-		req, err := http.NewRequest("GET", "http://169.254.169.254/latest/user-data", nil)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		req = req.WithContext(ctx)
-		resp, err := http.DefaultClient.Do(req)
-		if err == nil {
-			config := UserData{}
-			if err = json.NewDecoder(resp.Body).Decode(&config); err == nil {
-				serverID = config.ServerId
-			}
-		}
-	}
+	serverID := opts.ServerID
 
 	handler := snowstorm.Multi(serverID, 512)
 	http.ListenAndServe(fmt.Sprintf(":%v", opts.Port), handler)
